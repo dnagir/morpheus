4de34e4bf4fe4f541c56b6596e5c68db60d78c70
Dmytrii Nagirniak <dnagir@gmail.com>
2011-12-06 18:19:50 +1100

    mark up the spec to further work on

---
+++ spec/api/node_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Morpheus::API::Nodes do
+
+  describe "#get" do
+    it "should GET" do
+      fail
+    end
+
+    it "should create the model" do
+      fail
+    end
+  end
+
+  describe "#create" do
+    it "should POST"
+    it "should update the model"
+  end
+
+  describe "#update" do
+    it "should PUT"
+    it "should update the model"
+  end
+
+  describe "#delete" do
+    it "should DELETE"
+    it "should update the model"
+  end
+
+end
+
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,41 +1,29 @@
 require 'spec_helper'
 
-describe Morpheus::API::Nodes do
-  pending
-end
-
 describe Morpheus::API::Relationships do
-  pending
 end
 
 describe Morpheus::API::RelationshipTypes do
-  pending
 end
 
 describe Morpheus::API::RelationshipProperties do
 end
 
 describe Morpheus::API::Indexes do
-  pending
 end
 
 describe Morpheus::API::AutoIndexes do
-  pending
 end
 
 describe Morpheus::API::ConfigurableAutoIndexes do
-  pending
 end
 
 describe Morpheus::API::Algorithms do
-  pending
 end
 
 describe Morpheus::API::Batching do
-  pending
 end
 
 describe Morpheus::API::Gremlin do
-  pending
 end
 
