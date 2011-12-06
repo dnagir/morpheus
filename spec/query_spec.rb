0e066685d27ec3633bd33f0e9e969eb779e34e0a
Dmytrii Nagirniak <dnagir@gmail.com>
2011-12-06 15:00:50 +1100

    no need to make method_missing public

--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -14,10 +14,6 @@ module Morpheus
       })
     end
 
-    def method_missing(method, *args, &block)
-      add_path(method)
-    end
-
     def execute!
       q = self.to_query
       api = Morpheus::API.const_get(q.type.to_s.camelize).new
@@ -25,6 +21,11 @@ module Morpheus
     end
 
     protected
+
+    def method_missing(method, *args, &block)
+      add_path(method)
+    end
+
 
     def assemble_paths
       Kernel.raise "Nothing is defined for the query." if @paths.empty?
